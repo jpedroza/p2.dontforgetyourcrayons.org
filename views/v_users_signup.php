5840ab5cd5ce7bf05e881db1f334e12231c2e136
John Pedroza <mr.john.pedroza@gmail.com>
2013-11-11 15:28:03 -0700

    made fixes to v_users_signup (e.g., I forgor to close off the fields in the form with a forward slash

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -2,18 +2,15 @@
 
 <form method='POST' action='/users/p_signup'>
 
-        First Name <input type='text' name='first_name'><br>
-        Last Name <input type='text' name='last_name'><br>
-        Email <input type='text' name='email'><br>
-        Password <input type='password' name='password'><br>
-		<!-- added new fields short bio, time zone, and location-->
-		Tell Us A Few Words About Yourself <textarea rows="5" cols="300" name="aboutyou"><br>
-		Timezone <input type='text' name='timezone'><br>
-		Location <input type='text' name='location'><br>
-		
-		
-        
-        <input type='submit' value='Sign Up'>
+        First Name <input type='text' name='first_name' /><br>
+        Last Name <input type='text' name='last_name' /><br>
+        Email <input type='text' name='email' /><br>
+        Password <input type='password' name='password' /><br>
+		<!-- added new fields short bio, time zone, and location -->
+		Timezone <input type='text' name='timezone' /><br>
+		Location <input type='text' name='location' /><br>
+		Tell Us A Few Words About Yourself <textarea rows="5" cols="300" name="aboutyou" /><br>
+        <input type='submit' value='Sign Up' />
         
 
 </form>
