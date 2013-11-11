9382c4a7ee115aeb9758dfca7c89edbade592245
John Pedroza <mr.john.pedroza@gmail.com>
2013-11-11 16:31:51 -0700

    added v_users_profile.php for edit option as a plus 1 feature

---
+++ views/v_users_profile.php
@@ -0,0 +1,20 @@
+<?php if(isset($user_name)): ?>
+        <h1>This is the profile for <?=$user_name?></h1>
+<?php else: ?>
+        <h1>No user has been specified</h1>
+<?php endif; ?>
+
+<form method='POST' action='/users/p_signup'>
+
+        First Name <input type='text' name='first_name' /><br>
+        Last Name <input type='text' name='last_name' /><br>
+        Email <input type='text' name='email' /><br>
+        Password <input type='password' name='password' /><br>
+		<!-- added new fields short bio, time zone, and location -->
+		Timezone <input type='text' name='timezone' /><br>
+		Location <input type='text' name='location' /><br>
+		Tell Us A Few Words About Yourself <textarea rows="5" cols="70" name="aboutyou"></textarea><br>
+        <input type='submit' value='Update My Profile' />
+        
+
+</form>
