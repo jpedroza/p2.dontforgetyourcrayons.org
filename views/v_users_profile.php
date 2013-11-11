b0b852bcb861be09f8ace45055d165f99ae583fe
John Pedroza <mr.john.pedroza@gmail.com>
2013-11-11 16:52:39 -0700

    made changes to v_users_profile to build a form to make changes to profile...first steps were to modify it so that we can see if we have access to the user information

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,7 +1,7 @@
-<?php if(isset($user_name)): ?>
-        <h1>This is the profile for <?=$user_name?></h1>
+<?php if(isset($user_id)): ?> 
+        <h1>This is the profile for <?=$user->first_name?></h1>
 <?php else: ?>
-        <h1>No user has been specified</h1>
+        <h1>Log back in, something went wrong.</h1>
 <?php endif; ?>
 
 <form method='POST' action='/users/p_signup'>
