3ee0cbbbd460bbfdf19f90c240f9cabb36e3d77b
John Pedroza <mr.john.pedroza@gmail.com>
2013-11-11 15:12:42 -0700

    updated v_users-signup

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -6,6 +6,12 @@
         Last Name <input type='text' name='last_name'><br>
         Email <input type='text' name='email'><br>
         Password <input type='password' name='password'><br>
+		<!-- added new fields short bio, time zone, and location-->
+		Tell Us A Few Words About Yourself <textarea rows="5" cols="300" name="aboutyou"><br>
+		Timezone <input type='text' name='timezone'><br>
+		Location <input type='text' name='location'><br>
+		
+		
         
         <input type='submit' value='Sign Up'>
         
