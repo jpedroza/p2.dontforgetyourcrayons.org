4f24b2f7e682313eaeb1bbfb23746e0fd10eef26
John Pedroza <mr.john.pedroza@gmail.com>
2013-11-11 17:29:13 -0700

    fixed the text area fill in value by placing the tag <?=$user->aboutyou?> in between the two text area tags

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -11,7 +11,7 @@
         <!-- added new fields short bio, time zone, and location -->
 		Timezone <input type='text' name='timezone' value="<?=$user->timezone?>"/><br>
 		Location <input type='text' name='location' value="<?=$user->location?>"/><br>
-		Tell Us A Few Words About Yourself <textarea rows="5" cols="70" name="aboutyou" value="<?=$user->aboutyou?>"></textarea><br>
+		Tell Us A Few Words About Yourself <textarea rows="5" cols="70" name="aboutyou" ><?=$user->aboutyou?></textarea><br>
         <input type='submit' value='Update My Profile' />
         
 
